61ab6b283872463fdfc46b7eabb2beb65b0f01fe
Phil Birnie <phil@7thstreetweb.com>
2021-02-05 18:22:12 -0500

    Remove some bio info,

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,7 @@ import Stats from "../components/stats"
 const IndexPage = () => (
   <Layout>
     <SEO title="Phil Birnie runs for 365 Days in a Row" />
-    <p>Hello! My name is Phil.  I live in Worthington, Ohio.</p>
-    <p>Starting on January 1, 2021, I will attempt to run for 365 days in a row -- just for fun. Keep track of my progress below.</p>
+    <p>Starting on January 1, 2021, I will attempt to run for 365 days in a row and 1,000 miles -- just for fun. Keep track of my progress below.</p>
     <h3>Rules:</h3>
     <ul>
       <li>Run at least 0.5 miles per day</li>
