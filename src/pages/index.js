7159b89a3fbb017de5e407fbef381c97b59d7fdb
Phil Birnie <phil@7thstreetweb.com>
2021-08-25 22:46:12 -0400

    Adds Monthly Statistics

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Table from "../components/table"
 import Stats from "../components/stats"
+import MothlyStats from "../components/monthly-stats"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Phil Birnie runs for 365 Days in a Row" />
-    <p>Starting on January 1, 2021, I will attempt to run for 365 days in a row and 1,000 miles.</p>
-    <h3>Rules:</h3>
-    <ul>
-      <li>Run at least 0.5 miles per day</li>
-      <li>Runs than span midnight may count as both days.</li>
-    </ul>
-    <Stats />
-    <Table />
-  </Layout>
+    <Layout>
+        <SEO title="Phil Birnie runs for 365 Days in a Row" />
+        <p>Starting on January 1, 2021, I will attempt to run for 365 days in a row and 1,000 miles.</p>
+        <h3>Rules:</h3>
+        <ul>
+            <li>Run at least 0.5 miles per day</li>
+            <li>Runs than span midnight may count as both days.</li>
+        </ul>
+        <Stats />
+        <h3>Monthly Totals</h3>
+        <MothlyStats />
+        <Table />
+    </Layout>
 )
 
 export default IndexPage
--- src/utilities/aggregators.js
+++ src/utilities/aggregators.js
@@ -1,5 +1,48 @@
-const Sum = ( accumulator, currentValue ) => accumulator + currentValue;
+const Sum = (accumulator, currentValue) => accumulator + currentValue;
 
-const RoundFloat = ( num ) => Math.round( num * 100 ) / 100;
+const RoundFloat = (num) => Math.round(num * 100) / 100;
 
-export { Sum, RoundFloat };
+const SpliceByMonth = (accumulator, currentRun) => {
+
+    const month = parseInt(currentRun.date.substr(5, 2), 10);
+
+    if (!accumulator[month]) {
+        accumulator[month] = [];
+    }
+
+    accumulator[month].push(currentRun);
+    return accumulator;
+};
+
+const GetMonthName = (monthNumber) => {
+    switch (monthNumber) {
+    case 1:
+        return 'Jan';
+    case 2:
+        return 'Feb';
+    case 3:
+        return 'Mar';
+    case 4:
+        return 'Apr';
+    case 5:
+        return 'May';
+    case 6:
+        return 'Jun';
+    case 7:
+        return 'Jul';
+    case 8:
+        return 'Aug';
+    case 9:
+        return 'Sep';
+    case 10:
+        return 'Oct';
+    case 11:
+        return 'Nov';
+    case 12:
+        return 'Dec';
+    default:
+        return '';
+    }
+}
+
+export {Sum, RoundFloat, SpliceByMonth, GetMonthName};
